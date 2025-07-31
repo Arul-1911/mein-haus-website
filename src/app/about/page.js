748669be639cdf09d009128055b05ad18e897bf6
Arul <arulpraveen19112002@gmail.com>
2025-07-31 14:50:48 +0530

    folder structure updated

---
+++ src/app/about/page.js
@@ -0,0 +1,14 @@
+import About from "@/components/aboutpage/AboutPage";
+import React from "react";
+
+const Page = () => {
+  return (
+    <>
+      <div>
+        <About />
+      </div>
+    </>
+  );
+};
+
+export default Page;
